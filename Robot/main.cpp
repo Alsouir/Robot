0d7aa711338cf71cb327d23073f9d26c717d7a4f
Alsu_Ibragimova <61387926+Alsouir@users.noreply.github.com>
2024-09-24 17:33:19 +0300

    Update main.cpp

--- Robot/main.cpp
+++ Robot/main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/logo2.jpg"));
-    a.setApplicationName("Название приложения");
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
